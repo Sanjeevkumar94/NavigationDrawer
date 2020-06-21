df6cd12fba7458fd9884507dcc94b20908207848
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2020-06-21 18:09:49 +0530

    add toggle button on toolbar

--- app/src/main/java/com/example/navigationdrawerex/MainActivity.java
+++ app/src/main/java/com/example/navigationdrawerex/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.navigationdrawerex;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout mNavDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nav_drawer_layout);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Facebook");
+
+        mNavDrawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mNavDrawer,toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+
+        );
+
+        mNavDrawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mNavDrawer.isDrawerOpen(GravityCompat.START))
+            mNavDrawer.closeDrawer(GravityCompat.START);
+        else
+          super.onBackPressed();
     }
 }
