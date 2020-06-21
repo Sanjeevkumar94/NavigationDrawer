50681a877c7e7ca331af35b9fb6894791316a3e0
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2020-06-21 18:38:57 +0530

    add clicks on navigation

--- app/src/main/java/com/example/navigationdrawerex/MainActivity.java
+++ app/src/main/java/com/example/navigationdrawerex/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.navigationdrawerex;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -7,14 +8,18 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     DrawerLayout mNavDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nav_drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -30,6 +35,15 @@ public class MainActivity extends AppCompatActivity {
 
         mNavDrawer.addDrawerListener(toggle);
         toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        if(savedInstanceState==null)
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container,new FragmentHome())
+                .commit();
+
+        navigationView.setCheckedItem(R.id.home);
+
 
     }
 
@@ -39,5 +53,25 @@ public class MainActivity extends AppCompatActivity {
             mNavDrawer.closeDrawer(GravityCompat.START);
         else
           super.onBackPressed();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()){
+            case R.id.chat:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container,new FragmentChat())
+                        .commit();
+                break;
+
+                case R.id.home:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container,new FragmentHome())
+                        .commit();
+                break;
+        }
+        mNavDrawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
